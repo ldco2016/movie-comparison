164db81619ff1083315e6ade191fea58ca8b27e9
ldco2016 <renaissance.scholar2012@gmail.com>
2020-01-24 11:46:11 -0600

    Made widget reusable with other sources of data

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const fetchData = async searchTerm => {
-  const response = await axios.get("http://www.omdbapi.com/", {
-    params: {
-      apikey: "fd224926",
-      s: searchTerm
-    }
-  });
-
-  if (response.data.Error) {
-    return [];
-  }
-
-  return response.data.Search;
-};
-
 createAutoComplete({
   root: document.querySelector(".autocomplete"),
   renderOption(movie) {
@@ -24,6 +9,20 @@ createAutoComplete({
   },
   inputValue(movie) {
     return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: "fd224926",
+        s: searchTerm
+      }
+    });
+
+    if (response.data.Error) {
+      return [];
+    }
+
+    return response.data.Search;
   }
 });
 
