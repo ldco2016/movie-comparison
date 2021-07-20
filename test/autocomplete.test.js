175825dfbdeb94a3206e71f6964d69aca6b9e356
ldco2016 <renaissance.scholar2012@gmail.com>
2021-07-19 20:39:01 -0500

    Writing Assertions

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,1 +1,18 @@
-it("works!", () => {});
+it("shows an autocomplete widget", () => {
+  createAutoComplete({
+    root: document.querySelector("#target"),
+    fetchData() {
+      return [
+        {Title: "Avengers"},
+        {Title: "Terrific Toddlers"},
+        {Title: "Boss Baby"}
+      ];
+    },
+    renderOption(movie) {
+      return movie.Title;
+    }
+  });
+
+  const dropdown = document.querySelector('.dropdown');
+  expect(dropdown.className).not.to.include('is-active');
+});
