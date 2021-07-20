13af6f32da5d0ecdda55418929b218f73b23c51a
ldco2016 <renaissance.scholar2012@gmail.com>
2021-07-19 20:51:50 -0500

    Fake DOM events

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,4 +1,5 @@
-it("shows an autocomplete widget", () => {
+beforeEach(() => {
+  document.querySelector('#target').innerHTML = "";
   createAutoComplete({
     root: document.querySelector("#target"),
     fetchData() {
@@ -12,7 +13,20 @@ it("shows an autocomplete widget", () => {
       return movie.Title;
     }
   });
+});
 
+it("Dropdown starts closed", () => {
   const dropdown = document.querySelector('.dropdown');
   expect(dropdown.className).not.to.include('is-active');
+});
+
+it("After searching, dropdown opens up", () => {
+  const input = document.querySelector("input");
+  // set the value
+  input.value = 'avengers';
+  // trigger the event
+  input.dispatchEvent(new Event("input"));
+
+  const dropdown = document.querySelector('.dropdown');
+  expect(dropdown.className).to.include('is-active');
 });
